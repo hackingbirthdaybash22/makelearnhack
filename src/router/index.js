49c05674f1e229825cc8684fa979fb871dfa6c9f
franceneee <57038559+franceneee@users.noreply.github.com>
2022-07-31 02:26:35 +0800

    Add FAQ page to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Faq from '../views/Faq.vue';
 
 const routes = [
   {
@@ -8,14 +9,15 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/guide",
+    name: "FAQ",
+    component: Faq,
   },
+  {
+    path: "/faq",
+    name: "FAQ",
+    component: Faq,
+  }
 ];
 
 const router = createRouter({
