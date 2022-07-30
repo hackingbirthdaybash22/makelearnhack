90eea8214bb441433b39f1216dae4b5e07c6c7c9
franceneee <57038559+franceneee@users.noreply.github.com>
2022-07-30 12:45:14 +0800

    Add motto to the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="motto-container">
+      <h1 id="motto">
+        <p class="red">Make,&nbsp;</p>
+        <p class="yellow">Learn,&nbsp;</p>
+        <p class="blue">Hack</p>
+      </h1>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 };
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+#motto {
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 75px;
+  display: flex;
+  margin: 0;
+}
+
+#motto-container {
+  border: 1vw solid #000000;
+  border-radius: 46px;
+  padding: 0px 200px;
+  margin: 5vh;
+  max-width: 60vw;
+}
+
+.red {
+  color: rgba(231, 52, 39, 1);
+}
+
+.yellow {
+  color: rgba(248, 185, 42, 1);
+}
+
+.blue {
+  color: rgba(29, 83, 159, 1);
+}
