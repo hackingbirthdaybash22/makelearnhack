8339925914651c82f4fcb2b09a3d8a937eb13dc4
franceneee <57038559+franceneee@users.noreply.github.com>
2022-07-30 13:17:02 +0800

    Add tooltip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,15 @@
         <p class="blue">Hack</p>
       </h1>
     </div>
+    <p id="text">Your guide to&nbsp;
+      <span class="tooltip"><u id="hackathons"> hackathons.
+        <span class="tooltiptext">A hackathon is an event organized by
+            a company for tech enthusiasts putting up hardware or software-related challenges to be solved over a fixed
+            time duration, generally two days or more.
+        </span></u></span>
+    </p>
+
+    <button>Let's get started!</button>
   </div>
 </template>
 
@@ -58,3 +67,56 @@ export default {
 .blue {
   color: rgba(29, 83, 159, 1);
 }
+
+#text {
+  font-size: 75px;
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+}
+
+#hackathons:hover {
+  color: rgba(29, 83, 159, 1);
+  cursor: pointer;
+}
+
+/* Tooltip CSS gotten from w3schools*/
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  font-size: 15px;
+  visibility: hidden;
+  width: 550px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  top: 100%;
+  left: 0%;
+  margin-left: -60px;
+}
+
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent black transparent;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+
