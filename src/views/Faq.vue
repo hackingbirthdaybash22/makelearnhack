87076e2f86a662d5918407690a64c00ac3432120
franceneee <57038559+franceneee@users.noreply.github.com>
2022-07-31 02:26:21 +0800

    Add basic code for FAQ page

---
+++ src/views/Faq.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="faq">
+    <h1>FAQ</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FAQ"
+};
+</script>
+
+<style scoped>
+.faq {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+}
+
+.red {
+  color: rgba(231, 52, 39, 1);
+}
+
+.yellow {
+  color: rgba(248, 185, 42, 1);
+}
+
+.blue {
+  color: rgba(29, 83, 159, 1);
+}
+
+
+</style>
