d067204ccb1d0ece65ffa25ea7f7670dd62985bf
franceneee <57038559+franceneee@users.noreply.github.com>
2022-07-31 17:37:46 +0800

    Add assets for birthday card demo

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="navbar-brand position-absolute top-0 start-50 translate-middle-x"> 
+    <div
+      class="navbar-brand position-absolute top-0 start-50 translate-middle-x"
+    >
       <img id="site-logo" alt="site logo" src="./assets/icon.png" />&emsp;
       <a>Make, Learn, Hack &emsp;</a>
     </div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="spacer"/>
+    <div class="spacer" />
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <router-link to="/"><i class="fa-solid fa-house fa-2xl"></i></router-link>
-        </li>&emsp;
+          <router-link to="/"
+            ><i class="fa-solid fa-house fa-2xl"></i
+          ></router-link>
+        </li>
+        &emsp;
         <li class="nav-item">
-          <router-link to="guide"><i class="fa-solid fa-scroll fa-2xl"></i></router-link>
-        </li>&emsp;
+          <router-link to="guide"
+            ><i class="fa-solid fa-scroll fa-2xl"></i
+          ></router-link>
+        </li>
+        &emsp;
         <li class="nav-item">
-          <router-link to="demo"><i class="fa-solid fa-laptop-code fa-2xl"></i></router-link>
-        </li>&emsp;
+          <router-link to="demo"
+            ><i class="fa-solid fa-laptop-code fa-2xl"></i
+          ></router-link>
+        </li>
+        &emsp;
         <li class="nav-item">
-          <router-link to="faq"><i class="fa-solid fa-circle-question fa-2xl"></i></router-link>
+          <router-link to="faq"
+            ><i class="fa-solid fa-circle-question fa-2xl"></i
+          ></router-link>
         </li>
       </ul>
     </div>
@@ -30,7 +50,7 @@
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans');
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans");
 
 #app {
   font-family: Noto Sans, Avenir, Helvetica, Arial, sans-serif;
@@ -76,7 +96,8 @@ a.router-link-exact-active {
 }
 
 @media screen and (max-width: 600px) {
-  h1, p {
+  h1,
+  p {
     font-size: 30px;
   }
 
